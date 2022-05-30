8a3498d3b963c418cd428b7e9cafb886c18bcad7
Seas00n <happywyxwyx@foxmail.com>
2022-05-30 19:25:08 +0800

    1

--- motion_imitation/run.py
+++ motion_imitation/run.py
@@ -10,16 +10,27 @@ import random
 import tensorflow as tf
 import time
 import argparse
+from motion_imitation.envs import env_builder as env_builder
+from motion_imitation.learning import imitation_policies as imitation_policies
+from motion_imitation.learning import ppo_imitation as ppo_imitation
+
+
+
+
+ENABLE_ENV_RANDOMIZER = True
+
+
 
 def main():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--mode", dest="mode", type=str, default="train")
     arg_parser.add_argument("--motion_file", dest="motion_file", type=str,
-                        default="MyMotionImitation/data/motions/dog_pace.txt")
+                        default="motion_imitation/data/motions/dog_pace.txt")
     arg_parser.add_argument("--int_save_freq", dest="int_save_freq", type=int,
                             default=0)  # save intermediate model every n policy steps
     arg_parser.add_argument("--visualize", dest="visualize", action="store_true", default=False)
-    
+    args = arg_parser.parse_args()
+    enable_env_rand = ENABLE_ENV_RANDOMIZER and (args.mode != "test")
     return
 
 if __name__ =='__main__':
