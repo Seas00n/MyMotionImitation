08b58146020cc5875958639032da6db2abaa7700
Seas00n <happywyxwyx@foxmail.com>
2022-05-30 19:07:57 +0800

    1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MyMotionImitation
+## Training Example
+python3 motion_imitation/run.py --mode train --motion_file motion_imitation/data/motions/dog_pace.txt --int_save_freq 10000000 --visualize 
+### --mode: 训练或者测试
+### --motion_file: 需要模仿的数据
+### --int_save_freq:
+specifies the frequency for saving intermediate policies every n policy steps.
+### --visualize:是否可视化
--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+import os
+import inspect
+currentdir = '/home/wangyuxuan/MyMotionImitation'
+parentdir = '/home/wangyuxuan'
+# 动态修改搜索路径优先级，优先在parentdir中搜索
+os.sys.path.insert(0, parentdir)
 import numpy as np
 import os
 import random
@@ -6,7 +12,14 @@ import time
 import argparse
 
 def main():
-        
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--mode", dest="mode", type=str, default="train")
+    arg_parser.add_argument("--motion_file", dest="motion_file", type=str,
+                        default="MyMotionImitation/data/motions/dog_pace.txt")
+    arg_parser.add_argument("--int_save_freq", dest="int_save_freq", type=int,
+                            default=0)  # save intermediate model every n policy steps
+    arg_parser.add_argument("--visualize", dest="visualize", action="store_true", default=False)
+    
     return
 
 if __name__ =='__main__':
